89ab386eeee2ec2463db448de2894f86a1f4f883
irada-garayeva <irada.garayeva98@gmail.com>
2023-12-05 11:08:58 +0100

    Moving to the next question

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,7 @@ import Loader from "./Loader.js";
 import Error from "./Error.js";
 import StartScreen from "./StartScreen.js";
 import Question from "./Question.js";
+import NextButton from "./NextButton.js";
 
 const initialState = {
   questions: [],
@@ -12,6 +13,7 @@ const initialState = {
   status: "loading",
   index: 0,
   answer: null,
+  points: 0,
 };
 
 function reducer(state, action) {
@@ -33,13 +35,15 @@ function reducer(state, action) {
             : state.points,
       };
     }
+    case "nextQuestion":
+      return { ...state, index: state.index++, answer: null };
     default:
       throw new Error("Action is unknown");
   }
 }
 
 function App() {
-  const [{ questions, status, index, answer }, dispatch] = useReducer(
+  const [{ questions, status, index, answer, points }, dispatch] = useReducer(
     reducer,
     initialState
   );
@@ -61,11 +65,14 @@ function App() {
           <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
         )}
         {status === "active" && (
-          <Question
-            question={questions[index]}
-            dispatch={dispatch}
-            answer={answer}
-          />
+          <>
+            <Question
+              question={questions[index]}
+              dispatch={dispatch}
+              answer={answer}
+            />
+            <NextButton dispatch={dispatch} answer={answer} />
+          </>
         )}
       </Main>
     </div>
